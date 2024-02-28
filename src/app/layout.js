1ff99dccec7db2696a52f1c5392d177b3fc6618c
Pujon Das Auvi <auvipujondas@gmail.com>
2024-02-28 18:41:39 +0600

    Finished the app

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,9 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-
         {children}
-
       </body>
     </html>
   );
